ed01f5bbf1d54bece8a0296c72a3ea5a9c0834b2
John <jabatzoglou@ucmerced.edu>
2020-07-13 12:38:38 -0700

    Create calcVPD.m

---
+++ calcVPD.m
@@ -0,0 +1,12 @@
+function vpd=calcVPD(t,tdew);
+% send data in degrees C for t and tdew;
+% output is vpd in hPa
+
+% solve for vapor pressure at  temp
+ew = 6.1078*exp(17.27*t./(t + 237.3)); 
+
+% solve for vapor pressure at dewpoint temp
+
+ewd = 6.1078*exp(17.27*tdew./(tdew + 237.3));
+
+vpd=ew-ewd;
